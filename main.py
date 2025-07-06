3b4f4ee2e663793d063ceca62332506e37471ce0
r33drichards <rwendt1337@gmail.com>
2025-07-06 01:54:46 -0700

    various changes

--- main.py
+++ main.py
@@ -145,24 +145,10 @@ http {{
         namespace="default"
     )
     logger.info("created nginx deployment")
-    logger.info("creating nginx service")
-    # create a service to serve the nginx deployment
-    nginx_service = kubernetes.client.V1Service(
-        metadata=kubernetes.client.V1ObjectMeta(name=nginx_name),
-        spec=kubernetes.client.V1ServiceSpec(
-            selector={"app": nginx_name},
-            ports=[kubernetes.client.V1ServicePort(port=80)]
-        )
-    )
-    # create the service
-    kubernetes.client.CoreV1Api().create_namespaced_service(
-        body=nginx_service,
-        namespace="default"
-    )
-    logger.info("created nginx service")
-    
-    
-    
+
+
+
+
 # delete the deployment and service
 @kopf.on.delete('kopf.dev.llmrequests', 'v1', 'llmrequests')
 def delete_fn(body, name, namespace, logger, **kwargs):
@@ -198,18 +184,6 @@ def delete_fn(body, name, namespace, logger, **kwargs):
         if e.status != 404:
             raise
     logger.info("deleted nginx deployment")
-    logger.info("deleting nginx service")
-    try:
-        kubernetes.client.CoreV1Api().delete_namespaced_service(
-            name=f"{metadata_name}-nginx",
-            namespace="default"
-        )
-    except kubernetes.client.exceptions.ApiException as e:
-        if e.status != 404:
-            raise
-    logger.info("deleted nginx service")
-    # PVC deletion removed (not created in create handler)
-    # Custom object deletion removed (not needed, Kopf handles it)
 
 
 @kopf.on.create('kopf.dev.claud-code', 'v1', 'claud-code')
@@ -356,32 +330,6 @@ def create_fn(body, name, namespace, logger, **kwargs):
         namespace=namespace
     )
     logger.info("created deployment")
-    logger.info("creating service")
-    service = kubernetes.client.V1Service(
-        metadata=kubernetes.client.V1ObjectMeta(name=metadata_name),
-        spec=kubernetes.client.V1ServiceSpec(
-            selector={"app": metadata_name},
-            ports=[kubernetes.client.V1ServicePort(port=8080, target_port=8080, name="http")]
-        )
-    )
-    kubernetes.client.CoreV1Api().create_namespaced_service(
-        body=service,
-        namespace=namespace
-    )
-    logger.info("created service")
-    logger.info("creating code-server service")
-    code_server_service = kubernetes.client.V1Service(
-        metadata=kubernetes.client.V1ObjectMeta(name=f"{metadata_name}-code-server"),
-        spec=kubernetes.client.V1ServiceSpec(
-            selector={"app": metadata_name},
-            ports=[kubernetes.client.V1ServicePort(port=8081, target_port=8080, name="code-server")]
-        )
-    )
-    kubernetes.client.CoreV1Api().create_namespaced_service(
-        body=code_server_service,
-        namespace=namespace
-    )
-    logger.info("created code-server service")
     logger.info("creating nginx configmap")
     nginx_conf = f"""
 events {{}}
@@ -464,19 +412,6 @@ http {{
         namespace=namespace
     )
     logger.info("created nginx deployment")
-    logger.info("creating nginx service")
-    nginx_service = kubernetes.client.V1Service(
-        metadata=kubernetes.client.V1ObjectMeta(name=nginx_name),
-        spec=kubernetes.client.V1ServiceSpec(
-            selector={"app": nginx_name},
-            ports=[kubernetes.client.V1ServicePort(port=80, target_port=80, name="http")]
-        )
-    )
-    kubernetes.client.CoreV1Api().create_namespaced_service(
-        body=nginx_service,
-        namespace=namespace
-    )
-    logger.info("created nginx service")
 
 
 
@@ -497,26 +432,26 @@ def delete_fn(body, name, namespace, logger, **kwargs):
         if e.status != 404:
             raise
     logger.info("deleted deployment")
-    logger.info("deleting service")
+    logger.info("deleting nginx configmap")
     try:
-        kubernetes.client.CoreV1Api().delete_namespaced_service(
-            name=metadata_name,
+        kubernetes.client.CoreV1Api().delete_namespaced_config_map(
+            name=f"{metadata_name}-nginx-config",
             namespace=namespace
         )
     except ApiException as e:
         if e.status != 404:
             raise
-    logger.info("deleted service")
-    logger.info("deleting code-server service")
+    logger.info("deleted nginx configmap")
+    logger.info("deleting nginx deployment")
     try:
-        kubernetes.client.CoreV1Api().delete_namespaced_service(
-            name=f"{metadata_name}-code-server",
+        kubernetes.client.AppsV1Api().delete_namespaced_deployment(
+            name=f"{metadata_name}-nginx",
             namespace=namespace
         )
     except ApiException as e:
         if e.status != 404:
             raise
-    logger.info("deleted code-server service")
+    logger.info("deleted nginx deployment")
     logger.info("deleting pvc")
     try:
         kubernetes.client.CoreV1Api().delete_namespaced_persistent_volume_claim(
@@ -536,27 +471,6 @@ def delete_fn(body, name, namespace, logger, **kwargs):
         if e.status != 404:
             raise
     logger.info("deleted data pvc")
-    logger.info("deleted claud-code")
-    logger.info("deleting nginx deployment")
-    try:
-        kubernetes.client.AppsV1Api().delete_namespaced_deployment(
-            name=f"{metadata_name}-nginx",
-            namespace=namespace
-        )
-    except ApiException as e:
-        if e.status != 404:
-            raise
-    logger.info("deleted nginx deployment")
-    logger.info("deleting nginx service")
-    try:
-        kubernetes.client.CoreV1Api().delete_namespaced_service(
-            name=f"{metadata_name}-nginx",
-            namespace=namespace
-        )
-    except ApiException as e:
-        if e.status != 404:
-            raise
-    logger.info("deleted nginx service")
     logger.info("deleted claud-code")
     logger.info("deleted claud-code")
     
